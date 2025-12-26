ba129d8c6aa4b789004353ca05b62b779d8b82f0
braedonolsen <braedonolsen2027@u.northwestern.edu>
2025-12-26 02:29:43 -0500

    began data loading

---
+++ 0a_import_data.R
@@ -0,0 +1,12 @@
+# NFLPA Data Competition ----
+## Load and import datasets ----
+
+### load packages ----
+library(tidyverse)
+library(nflverse)
+
+load_schedules() |> 
+  view()
+
+load_injuries() |> 
+  view()
